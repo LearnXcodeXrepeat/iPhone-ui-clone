10b9630a9f099e2fbc33c7dc69cbe84918aa1899
viraljain7 <veerljain1234@gmail.com>
2024-06-21 11:56:59 +0530

    Add model

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
